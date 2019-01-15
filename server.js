7307657bc67939e39f0b4a3b589736b6a331f2a1
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-15 13:42:11 -0800

    feat: add socket.io middleware

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const app = require('./app');
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 console.log(publicPath);
 
-const port = process.env.PORT;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+const server = http.createServer(app)
+const io = socketIO(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
