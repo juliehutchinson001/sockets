09caf9d34a75a8ff652b6f71bbfa18f70e56aad4
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-14 20:37:41 -0800

    fix: changing in preparation for heroku deployment

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const setupEnvironmentVars = require('./config/config');
 
-setupEnvironmentVars();
 const app = express();
 
 // Setup middleware
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require('./app');
 const path = require('path');
 const express = require('express');
-const { connectMongoose } = require('./db/mongoose');
 
-connectMongoose();
 
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
