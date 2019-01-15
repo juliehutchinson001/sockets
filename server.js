0877fbe921591e2d3d42e5bd5ed35a084600751a
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-14 20:08:57 -0800

    fix: change file location

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const path = require('path');
+const express = require('express');
 const { connectMongoose } = require('./db/mongoose');
 
 connectMongoose();
 
 const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
 console.log(publicPath);
 
 const port = process.env.PORT;
