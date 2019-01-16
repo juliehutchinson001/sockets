2a2d4f64861be522a72194308926e5e718ee458a
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-16 12:23:09 -0800

    feat: serverside socket code

--- server.js
+++ server.js
@@ -12,12 +12,63 @@ const server = http.createServer(app)
 const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
+//server talking to client
 io.on('connection', socket => {
-  console.log('user connected!');
+  console.log('client connected!');
 
+  // socket.on('asdfadsfsadfsadfasdffdasdfasdfasfasd', newEmail => {
+  //   console.log(newEmail);
+  // });
+
+
+
+  setInterval(() => {
+    socket.emit('newMessage', 'Message from server');
+  }, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //server creating custom event
+  //the data is being sent from the server to the client
+  // socket.emit('newEmail', {
+  //   from: 'julie.hutchinson001@outlook.com',
+  //   text: 'this is a new email',
+  //   createAt: new Date,
+  // });
+
+  // socket.emit('createMessage', {
+  //   from: 'julie.hutchinson001@outlook.com',
+  //   text: 'new message sent!',
+  //   createAt: new Date,
+  // });
+
+
+
+  // socket.emit('newMessage', {
+  //   from: 'Julie',
+  //   text: 'see you then!',
+  //   createAt: new Date,
+  // });
+
+  //server losing connection of the client
   socket.on('disconnect', () => {
     console.log('client disconnected');
   });
+
+  // socket.on('createMessage', newMessage => {
+  //   console.log(newMessage);
+  // });
 });
+
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
