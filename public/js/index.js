29cf4ed39b83553636b6b879037b6e33201b4c63
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-19 23:22:48 -0800

    feat: add event listener for input

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,58 @@
-const socket = io();
+const request = require('request');
+const socket = io(); // eslint-disable-line no-undef
+const input = document.querySelector('input');
 
-socket.on('connect', function() {
+socket.on('connect', () => {
   console.log('connected to server');
 });
 
-socket.on('newUserMessage', function(message) {
+socket.on('newUserMessage', message => {
   const headerWelcome = document.querySelector('.app-alert');
   const welcomeText = message.text;
-  setTimeout(() => { headerWelcome.innerText = welcomeText }, 1500);
+  setTimeout(() => {
+    headerWelcome.innerText = welcomeText;
+  }, 1500);
   console.log(message);
 });
 
-socket.on('newEmailToEveryoneBut', function(message) {
+socket.on('newEmailToEveryoneBut', message => {
   const headerAlert = document.querySelector('.app-text');
   const welcomeText = message.text;
   console.log(message);
-  setTimeout(() => { headerAlert.innerText = welcomeText }, 1000);
-  setTimeout(() => { headerAlert.innerText = 'Chat Room' }, 3500);
+  setTimeout(() => {
+    headerAlert.innerText = welcomeText;
+  }, 1000);
+  setTimeout(() => {
+    headerAlert.innerText = 'Chat Room';
+  }, 3500);
 });
 
-const sendToServer = () => {
-  socket.on('createMessageTo', () => {
-    const newMessage = event.target.value;
-    socket.broadcast.emit('newMessage', {
-      from: 'User',
-      text: newMessage,
-      createdAt: new Date(),
-    });
+// the client sends a callback to ask the server confirmation about receiving a new message
+socket.emit('createMessage', { sender: 'Julie', text: 'Hello from Julie' }, (sender, data) => {
+  console.log(`${data} by ${sender}`);
+});
 
-    console.log(newMessage);
-  });
+const sendToServer = newMessage => {
+  socket.emit(
+    'newMessage',
+    { sender: 'User', text: newMessage, createdAt: new Date() },
+    (sender, data) => {
+      console.log(`${data} by ${sender}`);
+    }
+  );
+
+  console.log(newMessage);
 };
 
-socket.on('disconnect', function() {
+input.addEventListener('input', event => {
+  const inputValue = event.target.value;
+  sendToServer(inputValue);
+});
+
+socket.on('newMessage', message => {
+  console.log('new Message: ', message);
+});
+
+socket.on('disconnect', () => {
   console.log('disconnected from the server');
 });
