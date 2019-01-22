b59febfe3d15c887a399816753181df7f56f875c
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-21 20:19:10 -0800

    fix: change google map key path

--- server/utils/request-geolocation.js
+++ server/utils/request-geolocation.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const secretKey = require('../middleware/secret-keys');
 
-const getGeolocation = () => {
-  const url = `www.google.com`;
-  request(url, (error, response) => {});
-};
+const googleKey = secretKey.development.GOOGLE_MAPS_KEY;
+const location = '3796%20Bertini%20court%20california';
 
-module.exports = { getGeolocation };
+request(
+  {
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${googleKey}&location=${location}`,
+    json: true,
+  },
+  (error, response, body) => {
+    const latitude = body.results[0].locations[0].latLng.lat;
+    const longitude = body.results[0].locations[0].latLng.lng;
+
+    console.log(`Address: ${body.results[0].locations[0].street}`);
+    console.log(`Latitude: ${latitude}`);
+    console.log(`Longitude: ${longitude}`);
+    console.log(`--------------------------------`);
+    console.log(JSON.stringify(response, null, 2));
+    console.log(`--------------------------------`);
+    console.log(JSON.stringify(error, null, 2));
+  }
+);
