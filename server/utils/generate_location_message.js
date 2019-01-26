fed56b1399658b4315524294cded4722e3e787b1
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-26 11:24:51 -0800

    feat: generate function to locate latitude longitude of an user

--- server/utils/generate_location_message.js
+++ server/utils/generate_location_message.js
@@ -1,5 +1,9 @@
 const generateLocationMessage = (sender, latitude, longitude) => ({
-
+  sender,
+  url: `www.google.com/maps?q=${latitude},${longitude}`,
+  latitude,
+  longitude,
+  createdAt: new Date().getTime(),
 });
 
 module.exports = { generateLocationMessage };
