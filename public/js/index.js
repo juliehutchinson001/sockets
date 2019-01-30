bb3ca96f0ec00a964656e49fde92c8b0211d2c06
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-30 08:52:27 -0800

    refactor: Add Admin messages to chat interface

--- public/js/index.js
+++ public/js/index.js
@@ -4,52 +4,51 @@ const inputVal = document.getElementById('input-message');
 const locationButton = document.getElementById('send-location');
 const ul = document.getElementById('messages');
 
+const createNewMessageElement = () => {
+  const li = document.createElement('li');
+  li.classList.add('message');
+  return li;
+};
+
 socket.on('connect', () => {
   console.log(`connected to server`);
 });
 socket.on('newLocationMessage', message => {
   const formattedTime = moment(message.createdAt).format('h:mm a'); // eslint-disable-line no-undef
-  const li = document.createElement('li');
-  li.classList.add('message');
   const a = document.createElement('a');
   a.setAttribute('target', '_blank');
   a.setAttribute('href', message.url);
   a.innerText = `My current location (${formattedTime})`;
+
+  const li = createNewMessageElement();
   li.appendChild(a);
   ul.appendChild(li);
 });
 
 socket.on('newUserMessage', message => {
-  const headerWelcome = document.querySelector('.chat__text');
+  const headerWelcome = createNewMessageElement();
   const welcomeText = message.text;
   setTimeout(() => {
-    headerWelcome.innerText = welcomeText;
-  }, 1500);
-  setTimeout(() => {
-    headerWelcome.innerText = 'Chat-Room';
-  }, 4000);
+    headerWelcome.innerText = `Admin: ${welcomeText}`;
+    ul.appendChild(headerWelcome);
+  }, 1000);
   console.log(message);
 });
 
 socket.on('newEmailToEveryoneBut', message => {
-  const headerAlert = document.querySelector('.chat__alert');
+  const headerAlert = createNewMessageElement();
   const welcomeText = message.text;
   console.log(message);
   setTimeout(() => {
-    headerAlert.innerText = welcomeText;
+    headerAlert.innerText = `Admin: ${welcomeText}`;
+    ul.appendChild(headerAlert);
   }, 1000);
-  setTimeout(() => {
-    headerAlert.innerText = '';
-  }, 3500);
-});
-
-// the client sends a callback to ask the server confirmation about receiving a new message
-socket.emit('createMessage', { sender: 'Julie', text: 'Hello from Julie' }, (sender, data) => {
-  console.log(`${data} by ${sender}`);
 });
 
 const sendToServer = newMessage => {
+  // the client sends a callback to ask the server confirmation about receiving a new message
   socket.emit('createMessage', { sender: 'User', text: newMessage }, (sender, data) => {
+    inputVal.value = '';
     console.log(`${data} by ${sender}`);
   });
 
