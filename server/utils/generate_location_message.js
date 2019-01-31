55c886c704ca149d8256f438bdfcd101f080cf05
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-30 16:01:17 -0800

    refactor: update embedded url for iframe

--- server/utils/generate_location_message.js
+++ server/utils/generate_location_message.js
@@ -3,6 +3,7 @@ const moment = require('moment');
 const generateLocationMessage = (sender, latitude, longitude) => ({
   sender,
   url: `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`,
+  embedMap: `https://mapquest.com/embed/?center=${latitude},${longitude}2&zoom=14&maptype=map`,
   latitude,
   longitude,
   createdAt: moment().valueOf(),
