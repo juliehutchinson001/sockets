a125bca8d9d1a16fd1c05380439f95a0d01a474a
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-31 21:49:00 -0800

    feat: abstracting boilerplate code to format message in chat

--- public/js/helpers/message_template.js
+++ public/js/helpers/message_template.js
@@ -1,1 +1,13 @@
 import { getDate } from './get_date';
+
+const newMessageElement = () => {
+  const li = document.createElement('li');
+  li.classList.add('message');
+  return li;
+};
+
+const newLocationMessageElement = location => {
+
+};
+
+module.exports = { newMessageElement, newLocationMessageElement };
