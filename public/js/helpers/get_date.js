627a293caf516abfeed101891bfbb2a47503666a
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-31 21:39:43 -0800

    feat: function to abstract repeated code to display date

--- public/js/helpers/get_date.js
+++ public/js/helpers/get_date.js
@@ -1,5 +1,11 @@
 const getDate = () => {
+  const nowDate = new Date();
+  const formattedDate = nowDate.toString().slice(4, 10);
+  const formattedTime = nowDate.toLocaleTimeString();
 
+  const currentDateHtml = `<span>${formattedDate} ${formattedTime}</span>`;
+
+  return currentDateHtml;
 };
 
 module.exports = { getDate };
