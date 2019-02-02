19b379ed36ef3adef456c3ccf7e14633279a32c6
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-02-02 15:03:17 -0800

    init: user's class to keep code dry and store user's names

---
+++ server/utils/users.js
@@ -0,0 +1,28 @@
+/*
+[
+  {
+    id: '/#12poiajdspfoif',
+    name: 'Julie',
+    room: 'Main'
+  }
+  {
+    id: '/#12poiajdspfoif',
+    name: 'Julie',
+    room: 'Main'
+  }
+]
+*/
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
+class Users {
+  constructor() {
+    this.users = [];
+  }
+
+
+}
+
+module.exports = { Users };
