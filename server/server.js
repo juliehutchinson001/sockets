a2d30aa40d8316a76967109c66595984596ad9fb
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-02-02 22:21:42 -0800

    feat: enabling rooms

--- server/server.js
+++ server/server.js
@@ -14,6 +14,21 @@ const port = process.env.PORT || 3000;
 io.on('connection', socket => {
   console.log('client connected!');
 
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+
   // server emitting a message to a specific client
   io.emit('newUserMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
@@ -24,18 +39,28 @@ io.on('connection', socket => {
   );
 
   socket.on('createMessage', (message, callback) => {
+    const user = users.getUser(socket.id);
     console.log('createMessage ', message);
-    io.emit('newMessage', generateMessage(message.sender, message.text));
+    // io.emit('newMessage', generateMessage(message.sender, message.text));
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
 
     // this is the cb that assures the client that the server got the message
     callback(message.sender, 'Server got the message sent');
   });
 
   socket.on('createLocationMessage', (coords, callback) => {
-    socket.emit(
-      'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
-    );
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+    // socket.emit(
+    //   'newLocationMessage',
+    //   generateLocationMessage('Admin', coords.latitude, coords.longitude)
+    // );
     callback('Server got the location request sent');
   });
 
@@ -57,7 +82,15 @@ io.on('connection', socket => {
   });
 
   // server losing connection of the client
-  socket.on('disconnect', () => console.log('client disconnected'));
+  // socket.on('disconnect', () => console.log('client disconnected'));
+  socket.on('disconnect', () => {
+    const user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
 });
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
